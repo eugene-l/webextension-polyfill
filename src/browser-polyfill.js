9c2697aee5f432c051b744089ac6acb71c4b127f
Eugene <leonow.ea@yandex.ru>
2022-02-21 16:27:40 +0300

    second commit

--- src/browser-polyfill.js
+++ src/browser-polyfill.js
@@ -5,7 +5,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
-var variable = "Some string";
 
 if (typeof globalThis != "object" || typeof chrome != "object" || !chrome || !chrome.runtime || !chrome.runtime.id) {
   throw new Error("This script should only be loaded in a browser extension.");
